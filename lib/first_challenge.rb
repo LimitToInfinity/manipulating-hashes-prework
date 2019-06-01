9654e5fd3590c763fd95537bd28436f5ad748869
Ahmed Gaber <21stlimitless@gmail.com>
2019-06-01 19:36:50 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   contact.each do |name, info|
     info.each do |category, inside_info|
-      inside_info.delete_if("strawberry")
+      inside_info.delete_if( |value| value == "strawberry")
     end
   end
 
